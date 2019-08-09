ee00bf39f6bf12cc22bed85907878e028882968d
LoobySimilien <LoobySimilien@users.noreply.github.com>
2019-08-09 06:58:46 -0400

    updated code

---
+++ script_gallery.js
@@ -0,0 +1,27 @@
+// a fade in function for the images - will not be used currently
+
+$(document).ready(function(){
+  $("img").click(function(){
+    $("#gallery-images1").fadeIn();
+    $("#gallery-images2").fadeIn("slow");
+    $("#gallery-images3").fadeIn(3000);
+  });
+});
+
+// -----------------------------------------
+// another fade in function for the images
+
+<img src="..." id="myImage">
+
+
+<script type="text/javascript">
+jQuery(function(){
+
+   // Fade In
+   $("#myImage").fadeIn();
+
+   // Fade Out
+   //$("#myImage").fadeOut();
+
+});
+</script>
